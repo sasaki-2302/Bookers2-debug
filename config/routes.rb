a86c33b32f6da2b9be3f2b97d56a13df94a5cd46
sasaki022435 <124408816+sasaki022435@users.noreply.github.com>
2023-02-06 08:10:09 +0000

    [favorite]応用3

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resource :favorite, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
   resources :users, only: [:index,:show,:edit,:update]
