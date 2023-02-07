017b3b15ba58cdc06df0a17c6849c52ae947f579
sasaki022435 <124408816+sasaki022435@users.noreply.github.com>
2023-02-07 07:04:32 +0000

    [follow]応用4

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
     resource :favorite, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:index,:show,:edit,:update]
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
 
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
