2125f268abf7cb28a3419c349e77160497985529
sasaki-2302 <124408816+sasaki022435@users.noreply.github.com>
2023-02-11 03:27:33 +0000

    [Number of posts]応用7b

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
     @book = Book.new
+    @today_book =  @books.where(created_at: Time.zone.now.all_day)
+    @yesterday_book = @books.where(created_at: 1.day.ago.all_day)
+    @this_week_book = @books.where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day)
+    @last_week_book = @books.where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day)
   end
 
   def index
